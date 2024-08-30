b87865dcccab1283026b244421f2e89e46df6b3b
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-08-30 17:30:52 +0200

    server/connection to database

--- server/server.js
+++ server/server.js
@@ -1,17 +1,28 @@
 // index.js
 const express = require('express');
 const pool = require('./db');
+require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 // Example route to test database connection
-app.get('/test-db', async (req, res) => {
+app.get('/api/test-db', async (req, res) => {
   try {
     const result = await pool.query('SELECT NOW()');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+app.get('/api/news', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM news_articles');
     res.json(result.rows);
   } catch (err) {
     console.error(err);
