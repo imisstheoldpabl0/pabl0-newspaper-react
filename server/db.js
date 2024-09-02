cb9f411665c232af48a8d413b75ea7197e338a8c
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-09-02 16:49:33 +0200

    all works still

--- server/db.js
+++ server/db.js
@@ -14,7 +14,7 @@ pool.connect(function(err) {
   if (err) {
     return err
   } else {
-    console.log(`Connected to 'trade-boy' on PostgreSQL at ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} on ${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`);
+    console.log(`Connected to 'newspaper' on ElephantSQL at ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} on ${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`);
   }
 })
 
