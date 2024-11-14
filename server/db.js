9a2ba959ac26ca619c6154ccda5104f820f36954
Pablo Rubio <158491988+imisstheoldpabl0@users.noreply.github.com>
2024-11-14 11:24:08 +0000

    try

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: true
   }
 });
 
@@ -14,5 +14,7 @@ pool.connect(function(err) {
   if (err) throw err;
   console.log(`Connected to 'trade-boy' on PostgreSQL at ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} on ${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`);
 })
+
+// hello world
 
 module.exports = pool;
